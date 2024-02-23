e572b93657eb86abd1b4fda9121164e703445fee
kkkkkerry <zhangkairui65@gmail.com>
2024-02-23 03:22:06 -0500

    collectibles setup

--- Assets/03-Prototype1/Scripts/PlayerController.cs
+++ Assets/03-Prototype1/Scripts/PlayerController.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public float jumpAmount = 5.0f;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         rb = GetComponent<Rigidbody>();
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue)
@@ -21,15 +28,34 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode.Impulse);
+        }
     }
 }
